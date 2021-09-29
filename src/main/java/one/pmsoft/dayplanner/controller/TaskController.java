d096861a207c8a30402c8c21cbda1ec7f2f55495
michu9971 <qrk1@interia.pl>
2021-09-29 21:47:12 +0200

    Changed application.properties to yaml format. Added InfoController for tests.

--- src/main/java/one/pmsoft/dayplanner/controller/TaskController.java
+++ src/main/java/one/pmsoft/dayplanner/controller/TaskController.java
@@ -5,7 +5,6 @@ import one.pmsoft.dayplanner.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
